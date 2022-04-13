048e92822b894bb13390bf857bbd4d5220106679
Rodrigo Matos <rodrigo98matos@gmail.com>
2022-04-13 16:23:23 -0300

    BT

--- Unidade_Movel/unidademovel/unidademovel.ino
+++ Unidade_Movel/unidademovel/unidademovel.ino
@@ -16,6 +16,14 @@
 //#define E32_TTL_1W 
 #include "Arduino.h"
 #include "LoRa_E32.h"
+#include "SoftwareSerial.h"
+
+#define N 9                                    // bits da parte fracionaria
+#define K (1 << (N - 1))                       // fator para arredondamento
+#define FloatToQ(x) (int16_t)(x*(float)(1<<N)) // converte float para Q
+#define QToFloat(x) ((float)x/(float)(1<<N))   // converte Q para float
+
+
 void printHex(uint8_t num);
 
 // ---------- esp8266 pins --------------
@@ -28,8 +36,27 @@ void printHex(uint8_t num);
 
 SoftwareSerial ss(3,4);
 LoRa_E32 e32ttl(&ss, 2,5,6); // portas: RX TX AUX M1 M0
+SoftwareSerial bt(10,11);
 
-
+//SoftwareSerial ss(10,9);
+//LoRa_E32 e32ttl(&ss, 11,7,8); // portas: RX TX AUX M1 M0
+//Structs
+typedef struct {
+  int64_t lats,longs;
+  uint16_t dia, mes;
+  uint16_t ano;
+  uint16_t hrs,mins,seg;  
+}Gps_t;
+struct Upload {
+  uint16_t id_barco;
+  Gps_t gps1;
+  int32_t tensao;
+};
+struct beacon{
+  uint16_t cont, times;
+  int16_t temp;
+  int16_t umidade;
+}beacon_t;
 
 // -------------------------------------
 void printParameters(struct Configuration configuration);
@@ -37,80 +64,52 @@ void printModuleInformation(struct ModuleInformation moduleInformation);
 //The setup function is called once at startup of the sketch
 void setup()
 {
-	Serial.begin(9600);
-	while (!Serial) {
-	    ; // wait for serial port to connect. Needed for native USB
-    }
-	delay(100);
-
-	e32ttl.begin();
-
-//	e32ttl.resetModule();
-	// After set configuration comment set M0 and M1 to low
-	// and reboot if you directly set HIGH M0 and M1 to program
-  Serial.println("Configuração anterior:");
-	ResponseStructContainer c;
-	c = e32ttl.getConfiguration();
-	Configuration configuration = *(Configuration*) c.data;
-	printParameters(configuration);
-	c.close();
   
-	// ---------------------------
-	Serial.println();
-	
+  bt.begin(9600);
+ 
+  pinMode(12,INPUT_PULLUP);
+  Serial.begin(9600);
+  while (!Serial) {
+      ; // wait for serial port to connect. Needed for native USB
+    }
+  delay(100);
+
+  e32ttl.begin();
+
+//  e32ttl.resetModule();
+  // After set configuration comment set M0 and M1 to low
+  // and reboot if you directly set HIGH M0 and M1 to program
+  Serial.println("Configuração anterior:");
+  ResponseStructContainer c;
+  c = e32ttl.getConfiguration();
+  Configuration configuration = *(Configuration*) c.data;
+  printParameters(configuration);
+  c.close();
+  
+  // ---------------------------
+  Serial.println();
+  
   //e32ttl.setMode(MODE_2_POWER_SAVING);
   setLoraConfig();
   Serial.println("Start listening!");
+  Serial.println(sizeof(Upload));
 
 }
-//Structs
-typedef struct {
-  float lats,longs;
-  uint8_t dia, mes;
-  long unsigned int ano;
-  uint8_t hrs,mins,seg;  
-}Gps_t;
-struct Upload {
-  uint8_t id_barco;
-  Gps_t gps1;
-  float tensao;
-};
-struct beacon{
-  unsigned long cont, times;
-  float temp;
-  float pressao;
-}beacon_t;
+
 
 // The loop function is called in an endless loop
 void loop()
 {
-	if (e32ttl.available()  > 1){
-
-  uint8_t type[1];  
- // first part of structure
-  ResponseContainer rs = e32ttl.receiveInitialMessage(sizeof(type));
-  memcpy ( type, rs.data.c_str(), sizeof(type) );
-  if(type[0]==1){
-    ResponseStructContainer rsc = e32ttl.receiveMessage(sizeof(beacon));
-   struct beacon farol = *(beacon*) rsc.data;
-
-    Serial.println(String(type[0]) + "," + String(farol.cont) + "," + String(farol.times) + "," + String(farol.temp) + "," + String(farol.pressao));
-
-    rsc.close();
-    
-  }else if(type[0]==0 || type[0]==2){
-    ResponseStructContainer rsc = e32ttl.receiveMessage(sizeof(Upload));
-    struct Upload upload = *(Upload*) rsc.data;
- 
-  
-      Serial.println(String(type[0])+ "," + String(upload.id_barco) +  "," + String(upload.gps1.lats)+ "," + String(upload.gps1.longs) + "," + (int)upload.gps1.dia + "," + (int)upload.gps1.mes +","+ (int)upload.gps1.ano + "," + (int)upload.gps1.hrs + "," + (int)upload.gps1.mins + "," + (int) upload.gps1.seg+"," +  upload.tensao);  
-    
-   rsc.close();
-  }else{
-    Serial.println("Estrutura de dados não reconhecida!");
+  if (e32ttl.available()  > 1){
+  ResponseContainer rs = e32ttl.receiveMessage();
+  String message = rs.data;
+  Serial.println(rs.status.getResponseDescription());
+  Serial.println(message);
+  if(digitalRead(12)){
+    bt.println(message);
   }
-	
-}
+  }
+  
 }
 
 void printHex(uint8_t num) {
@@ -142,34 +141,34 @@ void setLoraConfig(){
 
 
 void printParameters(struct Configuration configuration) {
-	Serial.println("----------------------------------------");
+  Serial.println("----------------------------------------");
 
-	Serial.print(F("HEAD : "));  Serial.print(configuration.HEAD, BIN);Serial.print(" ");Serial.print(configuration.HEAD, DEC);Serial.print(" ");Serial.println(configuration.HEAD, HEX);
-	Serial.println(F(" "));
-	Serial.print(F("AddH : "));  Serial.println(configuration.ADDH, DEC);
-	Serial.print(F("AddL : "));  Serial.println(configuration.ADDL, DEC);
-	Serial.print(F("Chan : "));  Serial.print(configuration.CHAN, DEC); Serial.print(" -> "); Serial.println(configuration.getChannelDescription());
-	Serial.println(F(" "));
-	Serial.print(F("SpeedParityBit     : "));  Serial.print(configuration.SPED.uartParity, BIN);Serial.print(" -> "); Serial.println(configuration.SPED.getUARTParityDescription());
-	Serial.print(F("SpeedUARTDatte  : "));  Serial.print(configuration.SPED.uartBaudRate, BIN);Serial.print(" -> "); Serial.println(configuration.SPED.getUARTBaudRate());
-	Serial.print(F("SpeedAirDataRate   : "));  Serial.print(configuration.SPED.airDataRate, BIN);Serial.print(" -> "); Serial.println(configuration.SPED.getAirDataRate());
+  Serial.print(F("HEAD : "));  Serial.print(configuration.HEAD, BIN);Serial.print(" ");Serial.print(configuration.HEAD, DEC);Serial.print(" ");Serial.println(configuration.HEAD, HEX);
+  Serial.println(F(" "));
+  Serial.print(F("AddH : "));  Serial.println(configuration.ADDH, DEC);
+  Serial.print(F("AddL : "));  Serial.println(configuration.ADDL, DEC);
+  Serial.print(F("Chan : "));  Serial.print(configuration.CHAN, DEC); Serial.print(" -> "); Serial.println(configuration.getChannelDescription());
+  Serial.println(F(" "));
+  Serial.print(F("SpeedParityBit     : "));  Serial.print(configuration.SPED.uartParity, BIN);Serial.print(" -> "); Serial.println(configuration.SPED.getUARTParityDescription());
+  Serial.print(F("SpeedUARTDatte  : "));  Serial.print(configuration.SPED.uartBaudRate, BIN);Serial.print(" -> "); Serial.println(configuration.SPED.getUARTBaudRate());
+  Serial.print(F("SpeedAirDataRate   : "));  Serial.print(configuration.SPED.airDataRate, BIN);Serial.print(" -> "); Serial.println(configuration.SPED.getAirDataRate());
 
-	Serial.print(F("OptionTrans        : "));  Serial.print(configuration.OPTION.fixedTransmission, BIN);Serial.print(" -> "); Serial.println(configuration.OPTION.getFixedTransmissionDescription());
-	Serial.print(F("OptionPullup       : "));  Serial.print(configuration.OPTION.ioDriveMode, BIN);Serial.print(" -> "); Serial.println(configuration.OPTION.getIODroveModeDescription());
-	Serial.print(F("OptionWakeup       : "));  Serial.print(configuration.OPTION.wirelessWakeupTime, BIN);Serial.print(" -> "); Serial.println(configuration.OPTION.getWirelessWakeUPTimeDescription());
-	Serial.print(F("OptionFEC          : "));  Serial.print(configuration.OPTION.fec, BIN);Serial.print(" -> "); Serial.println(configuration.OPTION.getFECDescription());
-	Serial.print(F("OptionPower        : "));  Serial.print(configuration.OPTION.transmissionPower, BIN);Serial.print(" -> "); Serial.println(configuration.OPTION.getTransmissionPowerDescription());
+  Serial.print(F("OptionTrans        : "));  Serial.print(configuration.OPTION.fixedTransmission, BIN);Serial.print(" -> "); Serial.println(configuration.OPTION.getFixedTransmissionDescription());
+  Serial.print(F("OptionPullup       : "));  Serial.print(configuration.OPTION.ioDriveMode, BIN);Serial.print(" -> "); Serial.println(configuration.OPTION.getIODroveModeDescription());
+  Serial.print(F("OptionWakeup       : "));  Serial.print(configuration.OPTION.wirelessWakeupTime, BIN);Serial.print(" -> "); Serial.println(configuration.OPTION.getWirelessWakeUPTimeDescription());
+  Serial.print(F("OptionFEC          : "));  Serial.print(configuration.OPTION.fec, BIN);Serial.print(" -> "); Serial.println(configuration.OPTION.getFECDescription());
+  Serial.print(F("OptionPower        : "));  Serial.print(configuration.OPTION.transmissionPower, BIN);Serial.print(" -> "); Serial.println(configuration.OPTION.getTransmissionPowerDescription());
 
-	Serial.println("----------------------------------------");
+  Serial.println("----------------------------------------");
 
 }
 void printModuleInformation(struct ModuleInformation moduleInformation) {
-	Serial.println("----------------------------------------");
-	Serial.print(F("HEAD BIN: "));  Serial.print(moduleInformation.HEAD, BIN);Serial.print(" ");Serial.print(moduleInformation.HEAD, DEC);Serial.print(" ");Serial.println(moduleInformation.HEAD, HEX);
+  Serial.println("----------------------------------------");
+  Serial.print(F("HEAD BIN: "));  Serial.print(moduleInformation.HEAD, BIN);Serial.print(" ");Serial.print(moduleInformation.HEAD, DEC);Serial.print(" ");Serial.println(moduleInformation.HEAD, HEX);
 
-	Serial.print(F("Freq.: "));  Serial.println(moduleInformation.frequency, HEX);
-	Serial.print(F("Version  : "));  Serial.println(moduleInformation.version, HEX);
-	Serial.print(F("Features : "));  Serial.println(moduleInformation.features, HEX);
-	Serial.println("----------------------------------------");
+  Serial.print(F("Freq.: "));  Serial.println(moduleInformation.frequency, HEX);
+  Serial.print(F("Version  : "));  Serial.println(moduleInformation.version, HEX);
+  Serial.print(F("Features : "));  Serial.println(moduleInformation.features, HEX);
+  Serial.println("----------------------------------------");
 
 }
